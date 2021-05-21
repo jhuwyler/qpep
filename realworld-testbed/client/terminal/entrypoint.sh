de282d11d48f5d279f0a54543f69fa9548fca502
jhuwyler <julian.huwyler@gmail.com>
2021-05-21 17:52:29 +0200

    started complexer client setup

--- realworld-testbed/client/terminal/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-sysctl -w net.ipv4.ip_forward=1
-/sbin/iptables -A PREROUTING -t mangle -p tcp -i eth1 -j TPROXY --on-port 8080 --tproxy-mark 1
-/sbin/iptables -A PREROUTING -t mangle -p tcp -i eth0 -j TPROXY --on-port 8080 --tproxy-mark 1
-/sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-/sbin/ip rule add fwmark 1 lookup 100
-/sbin/ip route add local 0.0.0.0/0 dev lo table 100
-go run /root/go/src/qpep/main.go -client -gateway 34.65.137.121
