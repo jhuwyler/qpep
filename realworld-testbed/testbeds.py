5aaaf0cf28ef1c5d2cdf82aef7ebda0fb8dfa767
jhuwyler <julian.huwyler@gmail.com>
2021-05-26 09:34:59 +0200

    made testbed class for realworld

--- realworld-testbed/testbeds.py
+++ realworld-testbed/testbeds.py
@@ -11,49 +11,30 @@ load_dotenv()
 class RealWorldTestbed(object):
     def start_testbed(self):
         # First, shut down any old running testbeds
-        logger.debug("Shutting Down Previous Testbeds")
+        logger.debug("Shutting Down Previous Testbed: local")
         subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_LOCAL") ,"down"], stderr=subprocess.DEVNULL)
+        logger.debug("Shutting Down Previous Testbeds: remote")
+        subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER"), "-c", "cloud" ,"down"], stderr=subprocess.DEVNULL)
 
         logger.debug("Starting local Testbed Containers")
-
         # Start the docker containers
-        subprocess.call(["docker-compose", "-f", "./client/docker-compose.yml" , "up", "-d"], env=my_env)
+        subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_LOCAL") , "up", "-d"], env=my_env)
+        logger.debug("Starting remote Testbed Containers")
+        subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER"), "-c", "cloud"  , "up", "-d"], env=my_env)
 
-        # now that the network is running, it is possible to add ip routes from user terminal through the network
-        logger.debug("Connecting User Terminal to Satellite Spot Beam")
-        docker_client = docker.from_env()
-        terminal_container = docker_client.containers.get(os.getenv("ST_CONTAINER_NAME"))
-        terminal_container.exec_run("/sbin/ip route delete default")
-        terminal_container.exec_run("/sbin/ip route add default via " + str(os.getenv("GW_NETWORK_HEAD")) + ".0.3")
         logger.success("Real-world Testbed Running")
 
     def stop_testbed(self):
-        logger.debug("Shutting Down Previous Testbeds")
-        subprocess.call(["docker-compose", "down"])
-
-    def connect_terminal_workstation(self):
-        logger.debug("Starting User Workstation")
-        docker_client = docker.from_env()
-        workstation_container = docker_client.containers.get(os.getenv("WS_ST_CONTAINER_NAME"))
-        logger.debug("Adding External Route to Docker Host for GUI Services")
-        workstation_container.exec_run("ip route add " + str(self.host_ip) + " via " + str(os.getenv("GUI_NETWORK_HEAD"))+".0.1 dev eth1")
-        logger.debug("Connecting User Workstation to Satellite Router")
-        workstation_container.exec_run("ip route del default")
-        workstation_container.exec_run("ip route add default via " + str(os.getenv("ST_NETWORK_HEAD"))+".0.4")
-        logger.success("Client Workstation Connected to Satellite Network")
-
-    def connect_sitespeed_workstation(self):
-        logger.debug("Starting Sitespeed Workstation")
-        docker_client = docker.from_env()
-        sitespeed_container = docker_client.containers.get(os.getenv("SITESPEED_CONTAINER_NAME"))
-        sitespeed_container.exec_run("ip route del default")
-        sitespeed_container.exec_run("ip route add default via " + str(os.getenv("ST_NETWORK_HEAD"))+".0.4")
-        logger.success("Sitespeed Workstation Connected to Satellite Network")
+        logger.debug("Shutting Down Testbed: local")
+        subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_LOCAL") ,"down"], stderr=subprocess.DEVNULL)
+        logger.debug("Shutting Down Testbeds: remote")
+        subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER"), "-c", "cloud" ,"down"], stderr=subprocess.DEVNULL)
+    
 
 if __name__ == '__main__':
     #subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER") ,"down"], stderr=subprocess.DEVNULL)
-    #subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER") ,"up"], stderr=subprocess.DEVNULL)
-    docker_client = docker.DockerClient(base_url="ssh://julian_huwyler@cloud.jhuwyler.dev")
+    subprocess.call(["docker-compose", "-f", os.getenv("COMPOSE_SERVER"), "-c", "cloud" ,"up"], stderr=subprocess.DEVNULL)
+    docker_client_cloud = docker.DockerClient(base_url="ssh://julian_huwyler@cloud.jhuwyler.dev")
     for container in docker_client.containers.list():
         print(container.id)
 
